b9031a8d223c7fa05e27dcd4ed5f5b5268af3df0
BarbaraMoser <49961423+BarbaraMoser@users.noreply.github.com>
2021-05-16 12:23:40 -0300

    Finalizando layout 3 e 4 que faltavam

---
+++ app/src/main/java/com/example/exerciciolayouts/Exec03.kt
@@ -0,0 +1,11 @@
+package com.example.exerciciolayouts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Exec03 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exec03)
+    }
+}
